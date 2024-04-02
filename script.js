897c53ef2eddd5375a1408cf420a9bc4752d9507
DESKTOP-KA6S9B7\SUCCESS <mdsajidurrahman375@gmail.com>
2024-04-02 19:48:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imageslider(image){
+    document.querySelector(".pepsi").src=image;
+}
+function changecolor(color){
+    const x = document.querySelector('.sec');
+    x.style.background = color;
+}
